4b7e4ac5b2a3be2d200d7df0fe81fccb0ec8c944
Nathaniel Dasyam <ndasyam@malaghan.org.nz>
2022-10-17 16:15:53 +1300

    adding to test a push to remote

--- mars.sh
+++ mars.sh
@@ -3,3 +3,5 @@
 echo "Cold and dry, but everything is my favorite color"
 echo "The two moons may be a problem for Wolfman"
 echo "But the Mummy will appreciate the lack of humidity"
+
+echo "adding line to push to remote"
